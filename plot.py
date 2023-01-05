a3d9802fecd762991ffbb0a9db9f194e2e5e1f7f
David McFadzean <david.mcfadzean@indexexchange.com>
2023-01-04 20:17:29 -0500

    plot example

---
+++ plot.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+# The data to plot (in this case, just a straight line)
+y = [0, 1, 2, 3, 4]
+
+# Create a figure and axis
+fig, ax = plt.subplots()
+
+# Plot the data
+ax.plot(y)
+
+# Show the plot
+plt.show()
